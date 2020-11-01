f1633f5574ad9f120370f59fd379b716e2c22d0d
Alexander Dan <40121410+apdan510@users.noreply.github.com>
2020-11-01 10:08:34 -0800

    Add files via upload

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.5.16;
+
+contract SimpleStorage {
+  string ipfsHash;
+
+  function set(string x) public {
+    ipfsHash = x;
+  }
+
+  function get() public view returns (string) {
+    return ipfsHash;
+  }
+}
